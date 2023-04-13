198076b40828ff5907b75bef891f5fcf8da16a82
Five Pandas <amandapaul1223@gmail.com>
2023-04-13 13:44:23 -0600

    sticky notes added skills

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ function Nav(props) {
     return (
         <nav className="Nav">
             <a className="Name">Amanda Paul</a>
-            <a className="NavLinks" onClick={() => props.setCurrentPage('About Me')} >About Me</a>
+            <a className="NavLinks" onClick={() => props.setCurrentPage('About Me')}>About Me</a>
             <a className="NavLinks" onClick={() => props.setCurrentPage('Portfolio')}>Portfolio</a>
             <a className="NavLinks" onClick={() => props.setCurrentPage('Resume')}>Resume</a>
             <a className="NavLinks" onClick={() => props.setCurrentPage('Contact')}>Contact</a>
@@ -11,3 +11,4 @@ function Nav(props) {
 }
 
 export default Nav;
+
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,13 +1,36 @@
+import pdf from '../assets/APResume2023.pdf';
+
+
 function Resume() {
     return (
+        <div>
+        <div>
+        <div className="ButtonContainer">
+            <a href={pdf} target="_blank" className='ResumeButton'>Resume</a>
+        </div>
+        </div>
         <div className="Resume">
-        <aside>
-            <a href="">Resume</a>
-        </aside>
-            <a>Skills</a>
-            <a>Skills</a>
-            <a>Skills</a>
-            <a>Skills</a>
+        <ul>
+            <li>
+            <a href="#" className='Super'><p>Growing superpower with Git and Github!</p></a>
+            </li>
+            <li>
+            <a href="#" className='SkillsHeader'><p>Proficient and growing software development skills</p></a>
+            </li>
+            <li>
+            <a href="#" className='Backend'><p>MySql, Node.js, Express.js, and MongoDB</p></a>
+            </li>
+            <li>           
+            <a href="#" className='Frontend'><p>Javascript, React.js, CSS, HTML, JQuery</p></a>
+            </li>
+            <li>           
+            <a href="#" className='Character1'><p>naturally curious, lover of learning, adaptable</p></a>
+            </li>
+            <li>           
+            <a href="#" className='Character2'><p>thrives working in a team, creative problem solver</p></a>
+            </li>
+        </ul>
+        </div>
         </div>
     )
 }
